9a4492c17c459e20c827849e2b410cf74a72463f
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-11-28 19:49:57 +0530

    added new files in terraform repo

--- variables.tf
+++ variables.tf
@@ -33,3 +33,69 @@ variable "igw_tags" {
     default = {}
   
 }
+
+variable "public_subnets_cidr" {
+  type = list
+  validation {
+    condition = length(var.public_subnets_cidr) == 2
+    error_message = "Please give 2 public valid subnet CIDR"
+  }
+}
+
+variable "public_subnets_tags" {
+  default = {}
+}
+
+variable "private_subnets_cidr" {
+  type = list
+  validation {
+    condition = length(var.private_subnets_cidr) == 2
+    error_message = "Please give 2 private valid subnet CIDR"
+  }
+}
+
+variable "private_subnets_tags" {
+  default = {}
+}
+
+variable "database_subnets_cidr" {
+  type = list
+  validation {
+    condition = length(var.database_subnets_cidr) == 2
+    error_message = "Please give 2 database valid subnet CIDR"
+  }
+}
+
+variable "database_subnets_tags" {
+  default = {}
+}
+
+variable "nat_gateway_tags" {
+  default = {}
+}
+
+variable "public_route_table_tags" {
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  default = {}
+}
+
+variable "database_route_table_tags" {
+  default = {}
+}
+
+variable "is_peering_required" {
+  type = bool
+  default = false
+}
+
+variable "acceptor_vpc_id" {
+  type = string
+  default = ""
+}
+
+variable "vpc_peering_tags" {
+  default = {}
+}
